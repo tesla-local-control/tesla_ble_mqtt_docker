a4bb4c92f65442f34e57ba88ab603812f08d9e59
Iain Bullock <59146297+iainbullock@users.noreply.github.com>
2024-06-08 17:22:33 +0100

    Create config.sh
    Initial

---
+++ app/config.sh
@@ -0,0 +1,8 @@
+#!/bin/ash
+# Enter values for your particular configuration here
+
+# VIN for your car goes here
+VIN=1234567890
+
+# Change this to OPTIONS_COMPLETE=1 when ready to run
+OPTIONS_COMPLETE=0
