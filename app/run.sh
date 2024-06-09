8bff9e7b1d6ad24288ebf6ee7b35f67962af5d9f
Iain Bullock <59146297+iainbullock@users.noreply.github.com>
2024-06-09 16:53:31 +0100

    Update run.sh

--- app/run.sh
+++ app/run.sh
@@ -5,22 +5,21 @@ echo "tesla_ble_docker by Iain Bullock 2024 https://github.com/iainbullock/tesla
 echo "Inspiration by Raphael Murray https://github.com/raphmur"
 echo "Instructions by Shankar Kumarasamy https://shankarkumarasamy.blog/2024/01/28/tesla-developer-api-guide-ble-key-pair-auth-and-vehicle-commands-part-3"
 
-echo -e "Configuration Options are:"
+echo "Configuration Options are:"
 echo TESLA_VIN=$TESLA_VIN
 echo SSH_PORT=$SSH_PORT
 
-generate_tesla_keypair() {
-  echo -e "Generating the private key"
+if [ ! -f private.pem ]; then
+  echo "Private key not found, assuming this is the first run"
+  echo "Generating the private key"
   openssl ecparam -genkey -name prime256v1 -noout > private.pem
   cat private.pem
-  echo -e "Generating the public key"
+  echo "Generating the public key"
   openssl ec -in private.pem -pubout > public.pem
   cat public.pem
-}
-
-if [ ! -f /data/private.pem ]; then
-  echo -e "Private key not found, assuming this is the first run"
-  generate_tesla_keypair
 fi
 
-
+if [ -f public.pem ]; then
+  echo "Public key found, sending to vehicle using BLE"
+  tesla-control -ble add-key-request public.pem owner cloud_key
+fi
