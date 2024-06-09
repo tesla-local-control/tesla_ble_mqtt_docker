7d6ff54a3504e6163e193e859eee8d872113d966
Iain Bullock <59146297+iainbullock@users.noreply.github.com>
2024-06-09 09:48:35 +0100

    Update config.sh

--- app/config.sh
+++ app/config.sh
@@ -2,7 +2,7 @@
 # Enter values for your particular configuration here
 
 # VIN for your car goes here
-VIN=1234567890
+TESLA_VIN=1234567890
 
 # Change this to OPTIONS_COMPLETE=1 when ready to run
 OPTIONS_COMPLETE=0
