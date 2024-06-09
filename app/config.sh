33f318d5acece1d2b67554fa332af7b1d113133b
Iain Bullock <59146297+iainbullock@users.noreply.github.com>
2024-06-09 16:54:04 +0100

    Delete app/config.sh

--- app/config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/ash
-# Enter values for your particular configuration here, or set in docker-compose.yml
-
-# VIN for your car goes here
-TESLA_VIN='00000000000000000'
-
-# Port for the ssh connection to send commands
-SSH_PORT='14222'
