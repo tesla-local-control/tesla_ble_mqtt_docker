1798c0b717be0c9aab59a419ffc96bd59260e142
Iain Bullock <59146297+iainbullock@users.noreply.github.com>
2024-06-10 10:33:06 +0100

    Update discovery.sh

--- app/discovery.sh
+++ app/discovery.sh
@@ -1,11 +1,6 @@
-#!/bin/bash
+#!/bin/ash
 
-mqtt_ip=192.168.21.5
-mqtt_port=1883
-mqtt_user=hass
-mqtt_pwd=grekshye
-
-mosquitto_pub -h $mqtt_ip -p $mqtt_port -u $mqtt_user -P $mqtt_pwd -t homeassistant/button/tesla_ble/generate_keys/config -m \
+mosquitto_pub -h $MQTT_IP -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PWD -t homeassistant/button/tesla_ble/generate_keys/config -m \
  '{
   "command_topic": "tesla_ble/config",
   "device": {
@@ -22,7 +17,7 @@ mosquitto_pub -h $mqtt_ip -p $mqtt_port -u $mqtt_user -P $mqtt_pwd -t homeassist
   "unique_id": "tesla_ble_generate_keys"
  }' 
 
-mosquitto_pub -h $mqtt_ip -p $mqtt_port -u $mqtt_user -P $mqtt_pwd -t homeassistant/button/tesla_ble/deploy_key/config -m \
+mosquitto_pub -h $MQTT_IP -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PWD -t homeassistant/button/tesla_ble/deploy_key/config -m \
  '{
   "command_topic": "tesla_ble/config",
   "device": {
@@ -39,7 +34,7 @@ mosquitto_pub -h $mqtt_ip -p $mqtt_port -u $mqtt_user -P $mqtt_pwd -t homeassist
   "unique_id": "tesla_ble_deploy_key"
  }' 
 
-mosquitto_pub -h $mqtt_ip -p $mqtt_port -u $mqtt_user -P $mqtt_pwd -t homeassistant/button/tesla_ble/open_trunk/config -m \
+mosquitto_pub -h $MQTT_IP -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PWD -t homeassistant/button/tesla_ble/open_trunk/config -m \
  '{
   "command_topic": "tesla_ble/command",
   "device": {
@@ -55,7 +50,7 @@ mosquitto_pub -h $mqtt_ip -p $mqtt_port -u $mqtt_user -P $mqtt_pwd -t homeassist
   "unique_id": "tesla_ble_open_trunk"
  }' 
 
-mosquitto_pub -h $mqtt_ip -p $mqtt_port -u $mqtt_user -P $mqtt_pwd -t homeassistant/button/tesla_ble/charging-start/config -m \
+mosquitto_pub -h $MQTT_IP -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PWD -t homeassistant/button/tesla_ble/charging-start/config -m \
  '{
   "command_topic": "tesla_ble/command",
   "device": {
@@ -71,7 +66,7 @@ mosquitto_pub -h $mqtt_ip -p $mqtt_port -u $mqtt_user -P $mqtt_pwd -t homeassist
   "unique_id": "tesla_ble_charging-start"
  }' 
 
-mosquitto_pub -h $mqtt_ip -p $mqtt_port -u $mqtt_user -P $mqtt_pwd -t homeassistant/button/tesla_ble/charging-stop/config -m \
+mosquitto_pub -h $MQTT_IP -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PWD -t homeassistant/button/tesla_ble/charging-stop/config -m \
  '{
   "command_topic": "tesla_ble/command",
   "device": {
@@ -87,7 +82,7 @@ mosquitto_pub -h $mqtt_ip -p $mqtt_port -u $mqtt_user -P $mqtt_pwd -t homeassist
   "unique_id": "tesla_ble_charging-stop"
  }' 
 
-mosquitto_pub -h $mqtt_ip -p $mqtt_port -u $mqtt_user -P $mqtt_pwd -t homeassistant/number/tesla_ble/charging-set-amps/config -m \
+mosquitto_pub -h $MQTT_IP -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PWD -t homeassistant/number/tesla_ble/charging-set-amps/config -m \
  '{
   "command_topic": "tesla_ble/charging-amps",
   "device": {
