9d74ea65abc5e7946a502bcedcfbe33d521d873e
Iain Bullock <59146297+iainbullock@users.noreply.github.com>
2025-01-03 10:28:11 +0000

    Update libproduct.sh
    Allow float for BLE_CMD_RETRY_DELAY

--- libproduct.sh
+++ libproduct.sh
@@ -4,6 +4,7 @@ function validateEnvVars() {
   VIN_PATTERN='^([A-HJ-NPR-Z0-9]{17})(\|[A-HJ-NPR-Z0-9]{17})*$'
   INT0PLUS_PATTERN='^[0-9]+$'
   INT1PLUS_PATTERN='^[1-9][0-9]*$'
+  FLOAT_PATTERN='/[0-9]*\.?[0-9]+'
   # Hostname & IPv4 Address
   MQTT_SERVER_PATTERN='^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\b|\b([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$'
 
@@ -17,7 +18,7 @@ function validateEnvVars() {
     exitOnError=1
   fi
 
-  if ! echo $BLE_CMD_RETRY_DELAY | grep -Eq "$INT0PLUS_PATTERN"; then
+  if ! echo $BLE_CMD_RETRY_DELAY | grep -Eq "$FLOAT_PATTERN"; then
     log_fatal "Fatal; BLE_CMD_RETRY_DELAY:$BLE_CMD_RETRY_DELAY is not compliant, please check this setting"
     exitOnError=1
   fi
